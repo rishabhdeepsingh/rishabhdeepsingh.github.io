39014b1ee854e80cb162b6a9fa23b02a54cab991
Rishabhdeep Singh <rishabhdeepsingh98@gmail.com>
2019-09-04 02:09:47 +0530

    Basic SideBar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
+import {SideBar} from "./components/SideBar";
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    return (
+        <div className="App">
+            <SideBar onSelect={(selected) => {
+                console.log(selected.toString());
+            }}/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/SideBar.js
@@ -0,0 +1,35 @@
+import React, {Component} from "react";
+import SideNav, {NavIcon, NavItem, NavText} from "@trendmicro/react-sidenav";
+import HomeTwoToneIcon from '@material-ui/icons/HomeTwoTone';
+import PictureAsPdfTwoToneIcon from '@material-ui/icons/PictureAsPdfTwoTone';
+import '@trendmicro/react-sidenav/dist/react-sidenav.css';
+
+class SideBar extends Component {
+    render() {
+        return <SideNav
+            onSelect={this.props.onSelect}
+        >
+            <SideNav.Toggle/>
+            <SideNav.Nav defaultSelected="home">
+                <NavItem eventKey="home">
+                    <NavIcon>
+                        <HomeTwoToneIcon/>
+                    </NavIcon>
+                    <NavText>
+                        Home
+                    </NavText>
+                </NavItem>
+                <NavItem eventKey="resume">
+                    <NavIcon>
+                        <PictureAsPdfTwoToneIcon/>
+                    </NavIcon>
+                    <NavText>
+                        Resume
+                    </NavText>
+                </NavItem>
+            </SideNav.Nav>
+        </SideNav>;
+    }
+}
+
+export {SideBar}
