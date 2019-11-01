b971ab87ad587d932b95ce068d8f5a77fc6c8ea5
Restyled.io <commits@restyled.io>
2019-11-01 10:02:55 +0000

    Restyled by prettier

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,41 +1,39 @@
-import '@trendmicro/react-sidenav/dist/react-sidenav.css';
+import "@trendmicro/react-sidenav/dist/react-sidenav.css";
 
-import HomeTwoToneIcon from '@material-ui/icons/HomeTwoTone';
-import PictureAsPdfTwoToneIcon from '@material-ui/icons/PictureAsPdfTwoTone';
-import SideNav, {NavIcon, NavItem, NavText} from "@trendmicro/react-sidenav";
-import React, {Component} from "react";
+import HomeTwoToneIcon from "@material-ui/icons/HomeTwoTone";
+import PictureAsPdfTwoToneIcon from "@material-ui/icons/PictureAsPdfTwoTone";
+import SideNav, { NavIcon, NavItem, NavText } from "@trendmicro/react-sidenav";
+import React, { Component } from "react";
 
 class SideBar extends Component {
   render() {
-    return < SideNav
-    onSelect =
-        {this.props.onSelect} > <SideNav.Toggle />
-        <SideNav.Nav defaultSelected = "home"><NavItem eventKey = "home">
-        <NavIcon><HomeTwoToneIcon />
-        </NavIcon>
-                    <NavText>
-                        Home
-                    </NavText>
-        </NavItem>
-                <NavItem eventKey="resume.pdf">
-                    <NavIcon>
-                        <PictureAsPdfTwoToneIcon/>
-        </NavIcon>
-                    <NavText>
-                        Resume
-                    </NavText>
-        </NavItem>
-                <NavItem eventKey="Algovisual">
-                    <NavIcon>
-                        <PictureAsPdfTwoToneIcon/>
-        </NavIcon>
-                    <NavText>
-                        AlgoVisual
-                    </NavText>
-        </NavItem>
-            </SideNav.Nav><
-        /SideNav>
-    }
+    return (
+      <SideNav onSelect={this.props.onSelect}>
+        {" "}
+        <SideNav.Toggle />
+        <SideNav.Nav defaultSelected="home">
+          <NavItem eventKey="home">
+            <NavIcon>
+              <HomeTwoToneIcon />
+            </NavIcon>
+            <NavText>Home</NavText>
+          </NavItem>
+          <NavItem eventKey="resume.pdf">
+            <NavIcon>
+              <PictureAsPdfTwoToneIcon />
+            </NavIcon>
+            <NavText>Resume</NavText>
+          </NavItem>
+          <NavItem eventKey="Algovisual">
+            <NavIcon>
+              <PictureAsPdfTwoToneIcon />
+            </NavIcon>
+            <NavText>AlgoVisual</NavText>
+          </NavItem>
+        </SideNav.Nav>
+      </SideNav>
+    );
+  }
 }
 
-export {SideBar}
+export { SideBar };
--- src/components/resume.js
+++ src/components/resume.js
@@ -1,28 +1,28 @@
-import React, {Component} from 'react';
-import {Document, Page} from 'react-pdf';
+import React, { Component } from "react";
+import { Document, Page } from "react-pdf";
 
 class MyApp extends Component {
   state = {
-    numPages : null,
-    pageNumber: 1,
-  }
+    numPages: null,
+    pageNumber: 1
+  };
 
-  onDocumentLoadSuccess = ({numPages}) => { this.setState({numPages}); }
+  onDocumentLoadSuccess = ({ numPages }) => {
+    this.setState({ numPages });
+  };
 
   render() {
-    const {pageNumber, numPages} = this.state;
+    const { pageNumber, numPages } = this.state;
 
-        return (
-            <div>
-                <Document
-        file = "resume.pdf"
-                    onLoadSuccess={this.onDocumentLoadSuccess}
-                >
-                    <Page pageNumber={
-      pageNumber}/>
-                </Document>
-                <p>Page {pageNumber} of {numPages}</p>
-            </div>
-        );
+    return (
+      <div>
+        <Document file="resume.pdf" onLoadSuccess={this.onDocumentLoadSuccess}>
+          <Page pageNumber={pageNumber} />
+        </Document>
+        <p>
+          Page {pageNumber} of {numPages}
+        </p>
+      </div>
+    );
   }
 }
