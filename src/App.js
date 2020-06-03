1cf4c135448248136e5597fee55ce9a76af275a2
Rishabhdeep Singh <rishabhdeepsingh98@gmail.com>
2020-06-04 00:13:51 +0530

    Added algovisual icon

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import './App.css';
 import {SideBar} from "./components/SideBar";
 
 function App() {
-    return (
-        <div className="App">
-            <SideBar onSelect={(selected) => {
-                window.location.href = selected;
-                console.log(selected.toString());
-            }}/>
-        </div>
-    );
+  return (
+    <div className="App">
+      <SideBar onSelect={(selected) => {
+        window.location.href = selected;
+        console.log(selected.toString());
+      }}/>
+    </div>
+  );
 }
 
 export default App;
