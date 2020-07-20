8afb1441d8caf1cf0b9090eabf2c2d8af8790024
WhiteKnife <rishabhdeepsingh>
2020-07-21 05:21:13 +0530

    add local server and js-> jsx and images

---
+++ src/components/resume.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import { Document, Page } from "react-pdf";
+
+class MyApp extends Component {
+  state = {
+    numPages: null,
+    pageNumber: 1,
+  };
+
+  onDocumentLoadSuccess = ({ numPages }) => {
+    this.setState({ numPages });
+  };
+
+  render() {
+    const { pageNumber, numPages } = this.state;
+
+    return (
+      <React.Fragment>
+        <Document file="resume.pdf" onLoadSuccess={this.onDocumentLoadSuccess}>
+          <Page pageNumber={pageNumber} />
+        </Document>
+        <p>
+          Page {pageNumber} of {numPages}
+        </p>
+      </React.Fragment>
+    );
+  }
+}
