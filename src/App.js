ada6f79cf2d4d1a168d582014baa95c040b711f4
Restyled.io <commits@restyled.io>
2020-07-21 00:17:01 +0000

    Restyled by prettier

--- src/App.js
+++ src/App.js
@@ -2,17 +2,16 @@ import "./App.css";
 
 import React from "react";
 
-import {SideBar} from "./components/sideBar";
+import { SideBar } from "./components/sideBar";
 
 function App() {
   return (
     <div className="App">
       <SideBar
-        onSelect={
-    (selected) => {
-      window.location.href = selected;
-      console.log(selected.toString());
-    }}
+        onSelect={(selected) => {
+          window.location.href = selected;
+          console.log(selected.toString());
+        }}
       />
     </div>
   );
